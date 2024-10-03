e48d94b76a16d10c411311ff9fb04d46cecd47f2
wchwawa <wch19961116@gmail.com>
2024-10-03 16:31:56 +1000

    Implement Redis protocol parser!!!

--- app/server.go
+++ app/server.go
@@ -7,12 +7,10 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
